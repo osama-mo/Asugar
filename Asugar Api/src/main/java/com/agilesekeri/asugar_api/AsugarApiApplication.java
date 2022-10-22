a0d9ce530e64fcab96cf1be661c41cd98522070c
Enes <anas.abk2001@gmail.com>
2022-10-22 22:17:33 +0300

    First commit

---
+++ Asugar Api/src/main/java/com/agilesekeri/asugar_api/AsugarApiApplication.java
@@ -0,0 +1,14 @@
+package com.agilesekeri.asugar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class AsugarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsugarApiApplication.class, args);
+    }
+
+}
