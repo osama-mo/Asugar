adf6acf15009b1ff265fc2e88a382f44a3217722
anasa <anas.abk2001@gmai.com>
2022-12-01 22:52:46 +0300

    Database implementation of project table and connecting it with the users table with some security optimizations

---
+++ Asugar Api/src/main/java/com/agilesekeri/asugar_api/project/ProjectMemberRole.java
@@ -0,0 +1,6 @@
+package com.agilesekeri.asugar_api.project;
+
+public enum ProjectMemberRole {
+    ADMIN,
+    MEMBER
+}
---
+++ Asugar Api/src/main/java/com/agilesekeri/asugar_api/project/issue/Issue.java
@@ -0,0 +1,12 @@
+package com.agilesekeri.asugar_api.project.issue;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Issue {
+
+}
