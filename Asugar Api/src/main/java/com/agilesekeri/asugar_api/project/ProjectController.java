15c32eae0ce80c7e3d93cf76181e6cd0427344c6
Enes <anas.abk2001@gmail.com>
2022-12-19 22:12:53 +0300

    Merging viewMember method into getMembers

--- Asugar Api/src/main/java/com/agilesekeri/asugar_api/project/ProjectController.java
+++ Asugar Api/src/main/java/com/agilesekeri/asugar_api/project/ProjectController.java
@@ -25,7 +25,8 @@ public class ProjectController {
 
     @GetMapping(path = "/members")
     public void getMembers(@PathVariable Long projectId, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Set<AppUserEntity> members = projectService.getMemberSet(projectId);
+        Project project = projectService.getProject(projectId);
+        Set<AppUserEntity> members = project.getMembers();
         String username = appUserService.getJWTUsername(request);
         AppUserEntity issuer = appUserService.loadUserByUsername(username);
 
@@ -37,6 +38,13 @@ public class ProjectController {
                 userInfo.put("Last Name", user.getLastName());
                 userInfo.put("Email Address", user.getUsername());
                 list.add(userInfo);
+
+                if(project.getAdmin() == user)
+                    userInfo.put("Title", "Admin");
+                else if(project.getProductOwner() == user)
+                    userInfo.put("Title", "Product Owner");
+                else
+                    userInfo.put("Title", "Member");
             }
 
             response.setContentType(APPLICATION_JSON_VALUE);
@@ -72,38 +80,6 @@ public class ProjectController {
         AppUserEntity user = appUserService.loadUserByUsername(username);
         projectService.removeMember(projectId, user);
         return true;
-    }
-
-    @GetMapping(path = "/members/{username}")
-    public void viewMember(@PathVariable Long projectId, @PathVariable String username, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String issuerUsername = appUserService.getJWTUsername(request);
-        AppUserEntity issuer = appUserService.loadUserByUsername(issuerUsername);
-        Project project = projectService.getProject(projectId);
-        Set<AppUserEntity> members = project.getMembers();
-
-        if(!members.contains(issuer))
-            throw new IllegalCallerException("The issuer is not qualified for the operation");
-
-        AppUserEntity target = appUserService.loadUserByUsername(username);
-        if(members.contains(target)) {
-            Map<String, String> userInfo = new HashMap<>();
-            userInfo.put("First Name", target.getFirstName());
-            userInfo.put("Last Name", target.getLastName());
-            userInfo.put("Email Address", target.getUsername());
-
-            if(project.getAdmin() == target)
-                userInfo.put("Title", "Admin");
-            else if(project.getProductOwner() == target)
-                userInfo.put("Title", "Product Owner");
-            else
-                userInfo.put("Title", "Member");
-
-            response.setContentType(APPLICATION_JSON_VALUE);
-            new ObjectMapper().writeValue(response.getOutputStream(), userInfo);
-        }
-
-        else
-           throw new InvalidTransactionException("The user is not a member of the project");
     }
 
     @GetMapping(path = "/sprints")
