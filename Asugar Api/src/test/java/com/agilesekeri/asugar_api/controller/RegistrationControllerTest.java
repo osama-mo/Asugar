627af81823d1ba9b9914b062587e192bdfe715eb
Enes <anas.abk2001@gmail.com>
2022-12-23 02:58:30 +0300

    updating some tests

--- Asugar Api/src/test/java/com/agilesekeri/asugar_api/controller/RegistrationControllerTest.java
+++ Asugar Api/src/test/java/com/agilesekeri/asugar_api/controller/RegistrationControllerTest.java
@@ -1,17 +1,15 @@
-package com.agilesekeri.asugar_api.registration;
+package com.agilesekeri.asugar_api.controller;
 
-import com.agilesekeri.asugar_api.model.request.RegistrationRequest;
-import com.agilesekeri.asugar_api.repository.AppUserRepository;
-import com.agilesekeri.asugar_api.service.AppUserService;
-import com.agilesekeri.asugar_api.controller.RegistrationController;
 import com.agilesekeri.asugar_api.email.EmailSender;
 import com.agilesekeri.asugar_api.email.EmailValidator;
-import com.agilesekeri.asugar_api.repository.ProjectRepository;
-import com.agilesekeri.asugar_api.service.ProjectService;
+import com.agilesekeri.asugar_api.model.request.RegistrationRequest;
+import com.agilesekeri.asugar_api.repository.AppUserRepository;
 import com.agilesekeri.asugar_api.repository.RegistrationTokenRepository;
-import com.agilesekeri.asugar_api.service.RegistrationTokenService;
+import com.agilesekeri.asugar_api.service.AppUserService;
 import com.agilesekeri.asugar_api.service.RegistrationService;
+import com.agilesekeri.asugar_api.service.RegistrationTokenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +24,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.servlet.http.HttpServletResponse;
+
 @ContextConfiguration(classes = {RegistrationController.class, RegistrationService.class, AppUserService.class,
-        ProjectService.class, BCryptPasswordEncoder.class, EmailValidator.class, RegistrationTokenService.class})
+        BCryptPasswordEncoder.class, EmailValidator.class, RegistrationTokenService.class})
 @ExtendWith(SpringExtension.class)
 class RegistrationControllerTest {
     @MockBean
@@ -36,9 +36,6 @@ class RegistrationControllerTest {
     @MockBean
     private EmailSender emailSender;
 
-    @MockBean
-    private ProjectRepository projectRepository;
-
     @Autowired
     private RegistrationController registrationController;
 
@@ -46,11 +43,25 @@ class RegistrationControllerTest {
     private RegistrationTokenRepository registrationTokenRepository;
 
     /**
-     * Method under test: {@link RegistrationController#register(RegistrationRequest)}
+     * Method under test: {@link RegistrationController#confirm(String, HttpServletResponse)}
      */
     @Test
+    @Order(2)
+    void testConfirm() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/registration/confirm").param("token", "foo");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(registrationController)
+                .build()
+                .perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    /**
+     * Method under test: {@link RegistrationController#register(RegistrationRequest, HttpServletResponse)}
+     */
+    @Test
+    @Order(1)
     void testRegister() throws Exception {
-        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.get("/")
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.post("/registration")
                 .contentType(MediaType.APPLICATION_JSON);
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -59,7 +70,7 @@ class RegistrationControllerTest {
         ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(registrationController)
                 .build()
                 .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().isAccepted());
     }
 }
 
