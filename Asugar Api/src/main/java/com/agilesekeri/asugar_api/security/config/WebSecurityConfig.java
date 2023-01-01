185bc5e33d5bef29e0aff880b1738ccc5210560b
Enes <anas.abk2001@gmail.com>
2023-01-01 23:40:23 +0300

    Update WebSecurityConfig.java

--- Asugar Api/src/main/java/com/agilesekeri/asugar_api/security/config/WebSecurityConfig.java
+++ Asugar Api/src/main/java/com/agilesekeri/asugar_api/security/config/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package com.agilesekeri.asugar_api.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -25,7 +27,24 @@ public class WebSecurityConfig {
                 "/password_reset").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.apply(CustomDsl.customDsl());
-        http.cors();
+        http.cors().configurationSource(c -> {
+            CorsConfiguration corsCfg = new CorsConfiguration();
+
+            // All origins, or specify the origins you need
+            corsCfg.addAllowedOriginPattern( "*" );
+
+            // If you really want to allow all methods
+//            corsCfg.addAllowedMethod( CorsConfiguration.ALL );
+
+            // If you want to allow specific methods only
+            corsCfg.addAllowedMethod( HttpMethod.GET );
+            corsCfg.addAllowedMethod( HttpMethod.DELETE );
+            corsCfg.addAllowedMethod( HttpMethod.PUT );
+            corsCfg.addAllowedMethod( HttpMethod.POST );
+            // ...
+
+            return corsCfg;
+        });
 
         return http.build();
     }
